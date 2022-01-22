b70fb3a033d2fcb7816f47e7b012c84372e1a08e
answnsdud <98201688+answnsdud@users.noreply.github.com>
2022-01-22 19:00:18 +0900

    2022_01_22
    1. Program class
    2.. variable
    3. if
    4. Method

---
+++ UnityLesson_CSharp/UnityLesson_CSharp_Method/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+
+namespace UnityLesson_CSharp_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintHelloworld();
+            PrintSomething("asfq"); // 함수 호출에서 소괄호 안 내용은 매개변수(parameter)라고 한다.
+            bool tmpIsFineshed = false;
+            tmpIsFineshed = PrintSomethingAndReturnIsFinished("asgasgasg");
+            Console.WriteLine(tmpIsFineshed);
+        }
+
+        // 인자X, 반환X
+        static void PrintHelloworld()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        // 인자0 , 반환X
+        static void PrintSomething(string something) // 함수 정의에서 소괄호 안 내용은 인자(Argument)라고 한다.
+
+        {
+            PrintHelloworld();
+        }
+        static void PrintSomethingAndReturnIsFinished();
+
+        // 인자O , 반환0
+
+
+        /*반환형 함수이름(인자자료형 인자이름, 인자자료형 인자이름 ..)
+        {
+
+             함수 연산 내용
+
+             return 반환할 내용
+        }*/
+}
+}
+
+
+
+
+
+
+
